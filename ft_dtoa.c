9300f9ecd2a788ae72b729812b068e5ab51a0fc0
Jeremie GAUTHIER <jergauth@e2r3p7.42.fr>
2018-12-30 14:28:25 +0100

    fix dtoa.c

--- ft_dtoa.c
+++ ft_dtoa.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/30 13:35:24 by jergauth          #+#    #+#             */
-/*   Updated: 2018/12/30 13:35:25 by jergauth         ###   ########.fr       */
+/*   Updated: 2018/12/30 14:28:06 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,51 @@ static char		*print_decimal_part(char *s, unsigned short p)
 	return (s);
 }
 
+static void		round_this(double d, char *s)
+{
+	size_t	len;
+	size_t	ret;
+	size_t	ind;
+
+	len = ft_strlen(s);
+	ret = 1;
+	if ((d * 10) > 5.0)
+	{
+		ind = len - ret;
+		while (ind > 0)
+		{
+			ind = len - ret;
+			if (s[ind] + 1 != 58)
+			{
+				s[ind] += 1;
+				break ;
+			}
+			else if (s[ind] + 1 == 58)
+				s[ind] = '0';
+			ret++;
+		}
+	}
+}
+
+static char		*join_free(char *s1, char *s2)
+{
+	char	*ptr;
+
+	ptr = s1;
+	s1 = ft_strjoin(s1, s2);
+	ft_strdel(&ptr);
+	ft_strdel(&s2);
+	return (s1);
+}
+
 static char		*fill_str(double d, unsigned short precision, char *s)
 {
 	char			*s2;
-	char			*ptr;
 	long			l;
 	unsigned short	p;
 
+	if (precision <= 0)
+		round_this(d, s);
 	if (precision > 0)
 	{
 		if (!(s2 = (char*)malloc(sizeof(*s2) * (precision + 3))))
@@ -61,18 +99,15 @@ static char		*fill_str(double d, unsigned short precision, char *s)
 			d -= l;
 		}
 		print_decimal_part(s2, p);
+		s = join_free(s, s2);
 	}
-	ptr = s;
-	s = ft_strjoin(s, s2);
-	ft_strdel(&ptr);
-	ft_strdel(&s2);
 	return (s);
 }
 
 char			*ft_dtoa(double d, unsigned short precision)
 {
-	long			l;
-	char			*s;
+	long	l;
+	char	*s;
 
 	l = (long)d;
 	s = ft_ltoa_base(l, 10);
