ff4c6b174fb91151e05bcdfb1904098e86e6c85e
jeremie-gauthier <jeremiegthr@gmail.com>
2019-03-06 19:37:23 +0100

    norming

---
+++ ft_isoperator.c
@@ -0,0 +1,4 @@
+int		ft_isoperator(char c)
+{
+	return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%');
+}
--- ft_isspace.c
+++ ft_isspace.c
@@ -12,8 +12,8 @@
 
 int	ft_isspace(int c)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\r' ||
-		c == '\v' || c == '\f')
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\r'
+		|| c == '\v' || c == '\f')
 		return (1);
 	return (0);
 }
