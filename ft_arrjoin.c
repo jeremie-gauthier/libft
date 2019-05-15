6c38f00a2bfaa2483163ed189cec0693bc26690d
Jeremie <jeremiegthr@gmail.com>
2019-05-16 01:25:46 +0200

    add arrjoin ft

---
+++ ft_arrjoin.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+static size_t	sum_len_words(const char **arr)
+{
+	size_t	sum;
+
+	sum = 0;
+	while (*arr)
+	{
+		sum += ft_strlen(*arr);
+		arr++;
+	}
+	return (sum);
+}
+
+char	*ft_arrjoin(const char **arr, const int glue)
+{
+	char	*str;
+	size_t	len;
+
+	len = ft_arrlen((void*)arr) + sum_len_words(arr);
+	if (!(str = (char*)malloc(sizeof(*str) * len)))
+		return (NULL);
+	while (*arr)
+	{
+		while (**arr)
+			*str++ = *(*arr)++;
+		arr++;
+		*str++ = glue;
+	}
+	*(str - 1) = '\0';
+	return (str - len);
+}
+
